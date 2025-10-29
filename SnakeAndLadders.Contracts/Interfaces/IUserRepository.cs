480052f2b6b5c77142e127c23f72d92ddf13a731
Bunnn12 <zs23014092@estudiantes.uv.mx>
2025-10-29 15:46:42 -0600

    metodos para la foto, aun falta, foto del chat me refiero

--- SnakeAndLadders.Contracts/Interfaces/IUserRepository.cs
+++ SnakeAndLadders.Contracts/Interfaces/IUserRepository.cs
@@ -9,6 +9,7 @@ namespace SnakeAndLadders.Contracts.Interfaces {
         AccountDto GetByUsername(string username);
         ProfilePhotoDto GetPhotoByUserId(int userId);
         AccountDto UpdateProfile(UpdateProfileRequestDto request);
+        string GetAvatarIdByUserId(int userId);
     }
 
 }
